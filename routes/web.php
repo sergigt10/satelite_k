10309e8c4afcc96e87a23b383de1a43ae1de2916
sergigt10 <sergigenero@gmail.com>
2022-02-11 13:23:18 +0100

    fitxers extres

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeBackendController@index')->name('backend.index');
+// Route::get('/', 'HomeBackendController@index')->name('backend.index');
 
 Auth::routes();
 
