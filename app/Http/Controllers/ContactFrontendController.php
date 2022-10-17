0be4ea6767b7c0f25660b32e543b9e872d0b3bd6
sergigt10 <sergigenero@gmail.com>
2022-10-17 12:36:18 +0200

    formulari web nou

--- app/Http/Controllers/ContactFrontendController.php
+++ app/Http/Controllers/ContactFrontendController.php
@@ -30,7 +30,14 @@ class ContactFrontendController extends Controller
             'msg' => $request->msg
         ];
 
-        Mail::to('phxhollow13@hotmail.com')->send(new ContactMail($data));
+        $AdminMessage  = "Formulari de contacte WEB\n\n";
+        $AdminMessage .= "Nom i cogmons: ".utf8_decode($data['name'])."\n";
+        $AdminMessage .= "Correu: ".utf8_decode($data['mail'])."\n";
+        $AdminMessage .= "Comentaris: ".utf8_decode($data['msg'])."\n";
+    
+        mail("info@satelitek.com", "Formulari de contacte WEB", $AdminMessage, "From: ".$data['mail']);
+
+        // Mail::to('phxhollow13@hotmail.com')->send(new ContactMail($data));
 
         return back()->with(['message_mail' => trans('Missatge enviat correctament! En breu ens posarem en contacte. Gràcies')]);
 
