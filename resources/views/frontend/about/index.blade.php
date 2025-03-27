74ff1235b5638f98df99708dbf35668c173c28cb
sergigt10 <sergigenero@gmail.com>
2025-03-27 10:15:43 +0100

    qui som nou

--- resources/views/frontend/about/index.blade.php
+++ resources/views/frontend/about/index.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-    <div class="ps-about-intro">
+    {{-- <div class="ps-about-intro">
         <div class="ps-section__left">
             <div class="ps-section__content">
                 <h5>@lang('QUI SOM')</h5>
@@ -23,20 +23,30 @@
                 </figure>
             </div>
         </div>
-    </div>
-    <div class="ps-about-moments">
+    </div> --}}
+    <div class="ps-about-moments" style="padding-top: 0px">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
                     <div class="ps-section__header" style="max-width: 100%;">
-                        <h4>@lang("Diversitat musical")</h4>
-                        <p>@lang("Sota aquesta diversitat musical, formen o han format part de la companyia artistes de la talla de Fundació Tony Manero, Ulls de Bruixot, Love of Lesbian, Muchachito Bombo Infierno, Roger Mas, Sisa, Pau Riba, Judit Neddermann, Mayte Martín, Moncho, Che Sudaka, Manu Chao, Juanito Makandé, Kiko Veneno o Carlinhos Brown. Així com també formen part del catàleg els darrers treballs de Peret, Patriarques de la Rumba, o Green Valley, entre d'altres.<br><br> Fidel a la seva filosofia, Satélite K continua actualment donant suport a nous talents com BGKO, INTANA o Green Valley, en el que representa una producció limitada alhora que selecta, mentre que en termes de desenvolupament la companyia aposta per l'especialització i una forta orientació cap als serveis de suport als creadors.<br><br> Per tot això i més, Satélite K està considerada en el present com una companyia de referència a nivell independent. La seva visió de futur a nivell organitzatiu passa per una estructura interna flexible, amb una gran capacitat per a l'adaptació al canvi i mitjançant una atenció permanent cap al seu sector i les noves tecnologies.")</p>
+                        <h4>@lang("Qui som")</h4>
+                        <p>@lang("Satèl·lit K és una discogràfica especialitzada en la producció, promoció i desenvolupament de propostes artístiques eclèctiques i d'alt valor musical, així com la distribució digital i física. És un dels segells de referència a Espanya, reconegut pel seu compromís amb projectes de prestigi i el seu paper com a plataforma de suport i acompanyament per a artistes.")</p>
+                        <br>
+                        <p>@lang("Fundada el 1992, els seus primers treballs van sorgir amb motiu de les Olimpíades de Barcelona, ​​creant els tocs de lliurament de medalles i les fanfàrries de les cerimònies d'inauguració sota la direcció de Carles Santos.")</p>
+                        <br>
+                        <p>@lang("Al llarg dels anys, han format part del seu catàleg artistes com Fundación Tony Manero, Ojos de Brujo, Love of Lesbian, Muchachito Bombo Infierno, Roger Mas, Sisa, Pau Riba, Judit Neddermann, Mayte Martín, Moncho, Che Sudaka, Manu Chao, Juanito Makandé, Kiko Veneno o Carlinhos Brown. També ha publicat treballs de Peret, Patriarques de la Rumba i Green Valley, entre d'altres.")</p>
+                        <br>
+                        <p>@lang("A Satèl·lit K duem a terme un treball integral a cada llançament discogràfic, des de la planificació estratègica de singles d'avançament, àlbums i videoclips fins a la distribució física en vinil i CD, així com la digital. El nostre equip treballa per posicionar els llançaments a les millors playlists de les plataformes de streaming i maximitzar la seva repercussió. A més, comptem amb un departament editorial que gestiona sincronitzacions en anuncis, sèries i pel·lícules, ampliant les oportunitats per als nostres artistes. Complementant aquests esforços, el nostre departament de màrqueting digital dissenya estratègies per potenciar la visibilitat de cada projecte en xarxes socials i plataformes en línia.")</p>
+                        <br>
+                        <p>@lang("Fidel a la seva filosofia, Satèl·lit K segueix apostant per nous talents com BGKO, RAULE, Juan Medina, Pure Negga, Manteca, Paula Valls o Fades mantenint una producció selecta i un enfocament especialitzat en serveis de suport als creadors.")</p>
+                        <br>
+                        <p>@lang("A dia d'avui, Satèl·lit K és una referència dins l'àmbit independent. La seva visió de futur es basa en una estructura flexible, amb una gran capacitat d'adaptació i un enfocament constant en l'evolució del sector.")</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="ps-site-partners">
+    {{-- <div class="ps-site-partners">
         <div class="container">
             <div class="ps-section__content">
                 <a href="#"><img src="{{ asset('frontend/img/tsunami.png') }}" alt="Tsunami"></a>
@@ -46,5 +56,5 @@
                 <a href="https://rocketmusic.es/" target="_blank"><img src="{{ asset('frontend/img/rocketmusic.png') }}" alt="Rocket Music"></a>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
