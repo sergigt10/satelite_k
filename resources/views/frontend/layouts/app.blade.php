6bc5bb5476562427cf48cebcfbe5218a29a2b937
sergigt10 <sergigenero@gmail.com>
2025-04-10 10:41:30 +0200

    tag manager

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -47,6 +47,16 @@
     </script>
     <!-- Google tag (gtag.js) -->
 
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MDDJ9HBK');
+    </script>
+    <!-- End Google Tag Manager -->
+
     <!-- Meta Pixel Code -->
     <script>
         !function(f,b,e,v,n,t,s)
@@ -66,6 +76,12 @@
     <!-- End Meta Pixel Code -->
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDDJ9HBK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header class="header header--standard {{ (request()->is('/')) ? 'transparent' : '' }}" data-sticky="true">
     <div class="header__left"><a class="ps-logo" href="{{ route('frontend.inici.index') }}"><img src="{{ asset('frontend/img/logo.png') }}" alt="Satélite K"></a></div>
     <div class="header__right">
